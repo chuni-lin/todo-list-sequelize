6c6734a6e3e0a72b00a4f710c60c4b954f189638
chuni-lin <william811566@gmail.com>
2021-01-28 10:42:45 +0800

    feat: add routes & login, register.hbs

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,6 +39,7 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
